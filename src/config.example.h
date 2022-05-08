13db8f2c1565108f73ed5805776470b8bbf6ccc0
krobus00 <michaelglaxy@gmail.com>
2022-05-08 13:58:07 +0700

    [ADD] post data to service

---
+++ src/config.example.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID "WIFI SSID"
+#define WIFI_PASSWORD "WIFI PASSWORD"
+
+const char *HOST = "https://domain.com";
+const int PORT = 3000;
+const char *FINGERPRINT = "SHA1 FINGERPRINT";
+
+const char *BASE_URL = "https:/domain.com/";
+const char *NODE_ACCESS_TOKEN = "NODE ACCESS TOKEN";
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,110 @@
-#include "DHT.h"
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <config.h>
+
 #define DHTTYPE DHT22
 
-uint8_t DHTPin = D2;
+// PIN DEFINE
+uint8_t DHTPin = D3;
+uint8_t LED = D0;
+DHT dht(DHTPin, DHTTYPE);
 
 int timeSinceLastRead = 0;
 
-DHT dht(DHTPin, DHTTYPE);
+String BASE = BASE_URL;
+String ACCESS_TOKEN = NODE_ACCESS_TOKEN;
+
+void setupWIFI()
+{
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print("*");
+    }
+    Serial.println("");
+    Serial.println("WiFi connection Successful");
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+}
 
 void setup()
 {
     Serial.begin(115200);
+    pinMode(LED, OUTPUT);
+    setupWIFI();
     dht.begin();
 
     Serial.println("Device Started");
-    Serial.println("-------------------------------------");
-    Serial.println("Running DHT!");
-    Serial.println("-------------------------------------");
+    Serial.println("------------------");
+    Serial.println("Running node!");
+    Serial.println("------------------");
+}
+
+void postData(float humidity, float temperature, float heatIndex)
+{
+    String endpoint = BASE + "/sensors";
+    String res;
+    HTTPClient http;
+    WiFiClientSecure client;
+
+    client.setFingerprint(FINGERPRINT);
+    client.connect(HOST, PORT);
+
+    http.begin(client, endpoint);
+
+    http.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Authorization", "Bearer " + ACCESS_TOKEN);
+
+    String jsonparams;
+    DynamicJsonDocument buff(1024);
+
+    buff["humidity"] = humidity;
+    buff["temperature"] = temperature;
+    buff["heatIndex"] = heatIndex;
+
+    serializeJson(buff, jsonparams);
+
+    int httpCode = http.POST(jsonparams);
+    if (httpCode == 200)
+    {
+        digitalWrite(LED, LOW);
+        delay(100);
+        digitalWrite(LED, HIGH);
+        delay(100);
+        timeSinceLastRead += 200;
+    }
+    http.end();
 }
 
 void loop()
 {
-    if (timeSinceLastRead > 2000)
+    if (WiFi.status() == WL_CONNECTED)
     {
-        float h = dht.readHumidity();
-        float t = dht.readTemperature();
-        float f = dht.readTemperature(true);
 
-        if (isnan(h) || isnan(t) || isnan(f))
+        if (timeSinceLastRead > 2000)
         {
-            Serial.println("Failed to read from DHT sensor!");
+            float h = dht.readHumidity();
+            float t = dht.readTemperature();
+            float f = dht.readTemperature(true);
+
+            if (isnan(h) || isnan(t) || isnan(f))
+            {
+                Serial.println("Failed to read from DHT sensor!");
+                timeSinceLastRead = 0;
+                return;
+            }
+
+            float hic = dht.computeHeatIndex(t, h, false);
+
             timeSinceLastRead = 0;
-            return;
+            postData(h, t, hic);
         }
-
-        float hif = dht.computeHeatIndex(f, h);
-        float hic = dht.computeHeatIndex(t, h, false);
-
-        Serial.print("Humidity: ");
-        Serial.print(h);
-        Serial.print(" %\t");
-        Serial.print("Temperature: ");
-        Serial.print(t);
-        Serial.print(" *C ");
-        Serial.print(f);
-        Serial.print(" *F\t");
-        Serial.print("Heat index: ");
-        Serial.print(hic);
-        Serial.print(" *C ");
-        Serial.print(hif);
-        Serial.println(" *F");
-
-        timeSinceLastRead = 0;
+        delay(100);
+        timeSinceLastRead += 100;
     }
-    delay(100);
-    timeSinceLastRead += 100;
 }
